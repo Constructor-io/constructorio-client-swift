ddb344f2d6095fb62e89b29ee34edbcdfd6c222f
Nikola Markovic <nikola.markovic@toptal.com>
2018-04-05 18:17:54 +0200

    Implemented group tracking on select event

--- AutocompleteClientTests/Logic/Query/Builder/TrackAutocompleteClickRequestBuilderTests.swift
+++ AutocompleteClientTests/Logic/Query/Builder/TrackAutocompleteClickRequestBuilderTests.swift
@@ -46,6 +46,57 @@ class TrackAutocompleteClickRequestBuilderTests: XCTestCase {
         XCTAssertEqual(request.url, URL(string: "https://ac.cnstrc.com/autocomplete/\(encodedClickedItemName)/search?autocomplete_key=\(testACKey)&original_query=\(encodedSearchTerm)&tr=click&_dt=\(requestDate)"))
     }
     
-
+    func testTrackACClickBuilder_tappingOnItemWithGroup_SendsGroupNameAsQueryParameter() {
+        let groupName = "groupName1"
+        let groupID = "groupID2"
+        let groupPath = "path/to/group"
+        let group = CIOGroup(displayName: groupName, groupID: groupID, path: groupPath)
+        
+        let tracker = CIOAutocompleteClickTracker(searchTerm: searchTerm, clickedItemName: clickedItemName, sectionName: nil, group: group)
+        builder = TrackAutocompleteClickRequestBuilder(tracker: tracker, autocompleteKey: testACKey)
+        let request = builder.getRequest()
+        
+        let containsGroupName = URLComponents(url: request.url!, resolvingAgainstBaseURL: true)!.queryItems!.contains { (item) -> Bool in
+            return item.name == Constants.TrackAutocompleteResultClicked.groupName && item.value! == groupName
+        }
+        XCTAssertTrue(containsGroupName, "URL should contain a URL query item with group name if item in group is tapped on.")
+    }
     
+    func testTrackACClickBuilder_tappingOnItemWithGroup_SendsGroupIDAsQueryParameter() {
+        let groupName = "groupName1"
+        let groupID = "groupID2"
+        let groupPath = "path/to/group"
+        let group = CIOGroup(displayName: groupName, groupID: groupID, path: groupPath)
+        
+        let tracker = CIOAutocompleteClickTracker(searchTerm: searchTerm, clickedItemName: clickedItemName, sectionName: nil, group: group)
+        builder = TrackAutocompleteClickRequestBuilder(tracker: tracker, autocompleteKey: testACKey)
+        let request = builder.getRequest()
+        
+        let containsGroupID = URLComponents(url: request.url!, resolvingAgainstBaseURL: true)!.queryItems!.contains { (item) -> Bool in
+            return item.name == Constants.TrackAutocompleteResultClicked.groupID && item.value! == groupID
+        }
+        XCTAssertTrue(containsGroupID, "URL should contain a URL query item with group id if item in group is tapped on.")
+    }
+    
+    func testTrackACClickBuilder_tappingOnItemWithNoGroup_DoesNotSendGroupIDAsQueryParameter() {
+        let tracker = CIOAutocompleteClickTracker(searchTerm: searchTerm, clickedItemName: clickedItemName, sectionName: nil, group: nil)
+        builder = TrackAutocompleteClickRequestBuilder(tracker: tracker, autocompleteKey: testACKey)
+        let request = builder.getRequest()
+        
+        let containsGroupID = URLComponents(url: request.url!, resolvingAgainstBaseURL: true)!.queryItems!.contains { (item) -> Bool in
+            return item.name == Constants.TrackAutocompleteResultClicked.groupID
+        }
+        XCTAssertFalse(containsGroupID, "URL shouldn't contain a URL query item with group id if item outside a group is tapped on.")
+    }
+    
+    func testTrackACClickBuilder_tappingOnItemWithNoGroup_DoesNotSendGroupNameAsQueryParameter() {
+        let tracker = CIOAutocompleteClickTracker(searchTerm: searchTerm, clickedItemName: clickedItemName, sectionName: nil, group: nil)
+        builder = TrackAutocompleteClickRequestBuilder(tracker: tracker, autocompleteKey: testACKey)
+        let request = builder.getRequest()
+        
+        let containsGroupName = URLComponents(url: request.url!, resolvingAgainstBaseURL: true)!.queryItems!.contains { (item) -> Bool in
+            return item.name == Constants.TrackAutocompleteResultClicked.groupName
+        }
+        XCTAssertFalse(containsGroupName, "URL shouldn't contain a URL query item with group name if item outside a group is tapped on.")
+    }
 }
