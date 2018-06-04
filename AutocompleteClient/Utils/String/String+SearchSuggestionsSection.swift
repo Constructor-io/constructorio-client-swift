3eb77ce940f90d09967849c007bf5ce9fbd744e2
Nikola Markovic <nikola.markovic@toptal.com>
2018-06-04 07:52:29 +0200

    Fixed file header

--- AutocompleteClient/Utils/String/String+SearchSuggestionsSection.swift
+++ AutocompleteClient/Utils/String/String+SearchSuggestionsSection.swift
@@ -1,9 +1,9 @@
 //
 //  String+SearchSuggestionsSection.swift
-//  AutocompleteClient
+//  Constructor.io
 //
-//  Created by Nikola Markovic on 4/10/18.
-//  Copyright © 2018 xd. All rights reserved.
+//  Copyright © Constructor.io. All rights reserved.
+//  http://constructor.io/
 //
 
 import Foundation
