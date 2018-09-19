e6916b9ed11172839ae8eddc6a2bfadbf80b0be0
Nikola Markovic <nikola.markovic@toptal.com>
2018-09-19 12:14:10 +0200

    Removed keychain support for UUID

--- AutocompleteClient/FW/Logic/ClientID/ClientIDGenerator.swift
+++ AutocompleteClient/FW/Logic/ClientID/ClientIDGenerator.swift
@@ -10,19 +10,7 @@ import Foundation
 import UIKit
 
 class ClientIDGenerator: UserIDGenerator{
-
-    let keychainService = SimpleKeychainService()
-    
     func generateClientID() -> String?{
-        if let id = keychainService.load(name: Constants.ClientID.key){
-            return id
-        }else{
-            if let uuid = UIDevice.current.identifierForVendor?.uuidString{
-                self.keychainService.save(name: Constants.ClientID.key, value: uuid)
-                return uuid
-            }else{
-                return nil
-            }
-        }
+        return UIDevice.current.identifierForVendor?.uuidString
     }
 }
