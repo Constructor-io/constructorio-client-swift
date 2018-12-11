0e8561493168ca12f3627afca73b969ba5b3108c
Zubin Tiku <rubikzube@gmail.com>
2018-12-10 20:52:13 -0800

    Added default memberwise initializer (#69)
    Added default memberwise initializer

--- AutocompleteClient/FW/Logic/ABTesting/CIOABTestCell.swift
+++ AutocompleteClient/FW/Logic/ABTesting/CIOABTestCell.swift
@@ -12,4 +12,8 @@ public struct CIOABTestCell{
     let key: String
     let value: String
 
+    public init(key: String, value: String) {
+        self.key = key
+        self.value = value
+    }
 }
