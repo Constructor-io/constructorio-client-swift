a7776c47a2b5278d8d3e23475cf6c423ea4aeb45
Nikola Markovic <nikola.markovic.xd@gmail.com>
2019-10-23 21:34:34 +0200

    Added sources path

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ConstructorIO",
+    path: "AutocompleteClient/"
     products: [
         .library(name: "ConstructorAutocomplete", targets: ["AutocompleteClient"])
     ],
