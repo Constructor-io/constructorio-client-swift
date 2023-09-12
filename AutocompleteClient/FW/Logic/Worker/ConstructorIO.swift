c4983d0f70bdf5f83c2b2ffe3ea0e862537704d6
mocca102 <islaam.agamy@gmail.com>
2023-09-12 15:29:59 +0300

    Update example comments

--- AutocompleteClient/FW/Logic/Worker/ConstructorIO.swift
+++ AutocompleteClient/FW/Logic/Worker/ConstructorIO.swift
@@ -434,7 +434,7 @@ public class ConstructorIO: CIOSessionManagerDelegate {
      
      ### Usage Example: ###
      ```
-     constructorIO.trackBrowseResultsLoaded(filterName: "Category", filterValue: "Snacks", resultCount: 674, resultID: "179b8a0e-3799-4a31-be87-127b06871de2")
+     constructorIO.trackBrowseResultsLoaded(filterName: "Category", filterValue: "Snacks", resultCount: 674, customerIDs: ["1234567-AB", "1234765-CD", "1234576-DE"], resultID: "179b8a0e-3799-4a31-be87-127b06871de2")
      ```
      */
     public func trackBrowseResultsLoaded(filterName: String, filterValue: String, resultCount: Int, customerIDs: [String]? = nil, resultID: String? = nil, completionHandler: TrackingCompletionHandler? = nil) {
