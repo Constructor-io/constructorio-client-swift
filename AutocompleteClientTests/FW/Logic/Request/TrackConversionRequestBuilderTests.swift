88d421b01c4d7f76d6a084f701b54e7b7ccb02b8
ST P <57369888+stanlp1@users.noreply.github.com>
2024-05-06 09:14:50 -0700

    [CI-3389] CustomType for conversions (#211)
    * customType for conversions
    
    * remove extra line

--- AutocompleteClientTests/FW/Logic/Request/TrackConversionRequestBuilderTests.swift
+++ AutocompleteClientTests/FW/Logic/Request/TrackConversionRequestBuilderTests.swift
@@ -19,6 +19,8 @@ class TrackConversionRequestBuilderTests: XCTestCase {
     fileprivate let sectionName = "some section name@"
     fileprivate let conversionType = "like"
     fileprivate let revenue = 12.45
+    fileprivate let isCustomType = true
+    fileprivate let displayName = "bongo"
 
     fileprivate var encodedSearchTerm: String = ""
     fileprivate var encodedItemName: String = ""
@@ -105,7 +107,7 @@ class TrackConversionRequestBuilderTests: XCTestCase {
     }
 
     func testTrackConversionBuilder_WithConversionType() {
-        let tracker = CIOTrackConversionData(searchTerm: self.searchTerm, itemName: self.itemName, customerID: self.customerID, conversionType: self.conversionType)
+        let tracker = CIOTrackConversionData(searchTerm: self.searchTerm, itemName: self.itemName, customerID: self.customerID, conversionType: self.conversionType, displayName: self.displayName, isCustomType: self.isCustomType)
         builder.build(trackData: tracker)
         let request = builder.getRequest()
         let url = request.url!.absoluteString
@@ -119,6 +121,8 @@ class TrackConversionRequestBuilderTests: XCTestCase {
         XCTAssertEqual(payload?["item_name"] as? String, itemName)
         XCTAssertEqual(payload?["item_id"] as? String, customerID)
         XCTAssertEqual(payload?["search_term"] as? String, searchTerm)
+        XCTAssertEqual(payload?["is_custom_type"] as? Bool, isCustomType)
+        XCTAssertEqual(payload?["display_name"] as? String, displayName)
     }
 
     func testTrackConversionBuilder_WithRevenue() {
