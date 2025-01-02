ea2d3d078a7bf7dc6a1eddbcb6d118a58938ce58
Islam Moustafa <58053149+mocca102@users.noreply.github.com>
2025-01-02 21:08:40 +0200

    Update search events group Id documentation (#229)
    * Update search events group Id documentation
    
    * update description

--- AutocompleteClient/FW/Logic/Worker/ConstructorIO.swift
+++ AutocompleteClient/FW/Logic/Worker/ConstructorIO.swift
@@ -348,7 +348,7 @@ public class ConstructorIO: CIOSessionManagerDelegate {
         - searchTerm: The term that the user selected
         - originalQuery: The current text in the input field
         - sectionName: The name of the autocomplete section the term came from (usually "Search Suggestions")
-        - group: Item group (if present)
+        - The group to search within. Only required if searching within a group, i.e. "Pumpkin in Canned Goods"
         - resultID: Identifier of result set
         - completionHandler: The callback to execute on completion.
 
@@ -369,7 +369,7 @@ public class ConstructorIO: CIOSessionManagerDelegate {
      - Parameters:
         - searchTerm: The term that the user searched for
         - originalQuery: The current text in the input field
-        - group: Item group (if present)
+        - The group to search within. Only required if searching within a group, i.e. "Pumpkin in Canned Goods"
         - completionHandler: The callback to execute on completion.
 
      ### Usage Example: ###
